786868930cae110865bbfc32d366ed0696becc82
Marcelo Matos <marcelomatos.dev@gmail.com>
2020-09-23 14:23:30 -0500

    Lesson19 - JS

---
+++ Lesson19/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+
+const MONTH_BIRTHDAY = 'March';
+const FIRST_NAME = 'Marcelo';
+const LAST_NAME = 'Matos';
+
+let city;
+let age;
+let nextCity;
+
+city = 'Winnipeg';
+age = 18;
+nextCity = city;
+
+console.log('Month of birthday: ', MONTH_BIRTHDAY);
+console.log(`Full Name: ${FIRST_NAME} ${LAST_NAME}`);
+
+console.log('City: ', city);
+console.log('Age: ', age);
+console.log('Next City: ', nextCity);
