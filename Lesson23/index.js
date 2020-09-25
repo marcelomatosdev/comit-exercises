c0ada85172351b09df7a42ea85c331addc07b996
Marcelo Matos <marcelomatos.dev@gmail.com>
2020-09-25 18:51:51 -0500

    Lesson23-JS

---
+++ Lesson23/index.js
@@ -0,0 +1,10 @@
+'use strict';
+let hour = Number(prompt("Please enter the current hour", 0))
+
+if(hour < 0 || hour > 23) {
+  alert('Please enter a number between 0 and 23');
+}else if(hour < 8 || hour > 18) {
+  alert('This hour is outside of office hours');
+}else{
+  alert("We are open!");
+}
