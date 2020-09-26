28ff323a320693252b20d87f7de45bda40710c21
Marcelo Matos <marcelomatos.dev@gmail.com>
2020-09-25 19:00:03 -0500

    Fixed the last condition adding the AND operator

--- Lesson23/index.js
+++ Lesson23/index.js
@@ -5,6 +5,6 @@ if(hour < 0 || hour > 23) {
   alert('Please enter a number between 0 and 23');
 }else if(hour < 8 || hour > 18) {
   alert('This hour is outside of office hours');
-}else{
+}else if(hour >= 8 && hour <= 18){
   alert("We are open!");
 }
